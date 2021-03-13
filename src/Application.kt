d14a56d007cfbb40f4f7ad26192066d839226ed3
Tadas <tadasgailevicius@yahoo.com>
2021-03-13 18:29:19 +0200

    Update Application.kt

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,30 @@
 package com.example
 
+import com.example.routes.postRoutes
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
+    install(Routing) {
+        postRoutes()
+    }
+
 }
 
